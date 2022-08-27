32bc1260bd8f5139a6a1aab1d1e67ecf3eba86ce
AliciadW <alicia.dewet@living-stratus.com>
2022-08-27 13:03:28 +0100

    :construction: add posts and post files; redirect, dynamic routes

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,9 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My first blog post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
+        <?= $post; ?>
     </article>
 
     <a href="/">Go Home</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,23 +5,20 @@
 
 <body>
     <article>
-        <h1><a href="/post">My first blog post</a></h1>
+        <h1><a href="/posts/my-first-post">My first blog post</a></h1>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
-
     </article>
 
     <article>
-        <h1><a href="/post">My second blog post</a></h1>
+        <h1><a href="/posts/my-second-post">My second blog post</a></h1>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
-
     </article>
 
     <article>
-        <h1><a href="/post">My third blog post</a></h1>
+        <h1><a href="/posts/my-third-post">My third blog post</a></h1>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
-
     </article>
 </body>

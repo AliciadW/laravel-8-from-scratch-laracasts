357de98d676a7f9395d225f2f0fcad0e2f80c06e
AliciadW <alicia.dewet@living-stratus.com>
2022-08-27 12:48:01 +0100

    :construction: add posts and post files; basic setup

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<title>Single post</title>
+<link rel="stylesheet" href="/app.css"/>
+
+<body>
+    <article>
+        <h1><a href="/post">My first blog post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
+    </article>
+
+    <a href="/">Go Home</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css"/>
+
+<body>
+    <article>
+        <h1><a href="/post">My first blog post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
+
+    </article>
+
+    <article>
+        <h1><a href="/post">My second blog post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
+
+    </article>
+
+    <article>
+        <h1><a href="/post">My third blog post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
+
+    </article>
+</body>

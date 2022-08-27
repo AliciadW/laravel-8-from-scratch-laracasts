0b5cd5676217a83c381f7b48f396e40ae8fd3823
AliciadW <alicia.dewet@living-stratus.com>
2022-08-27 15:18:38 +0100

    :recycle: Refactor code; add Post model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,15 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), function () use ($path) {
-       return file_get_contents($path);
-    });
-
     return view('post', [
-        'post' => $post
+        'post' => Post::find($slug)
     ]);
-})-> where('post', '[A-z_\-]+');
+})->where('post', '[A-z_\-]+');

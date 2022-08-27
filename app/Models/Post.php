d8f866dde36c6634846fe4543d4a5867fcb0312d
AliciadW <alicia.dewet@living-stratus.com>
2022-08-27 15:47:59 +0100

    :recycle: Refactor code

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,13 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
 class Post
 {
 
-    public static function find($slug) {
+    public static function all(): array
+    {
+        $files = File::files(resource_path("posts/"));
 
-        if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+
+        // arrow function
+        // return array_map(fn($file)=>$file->getContents(), $files);
+    }
+
+    public static function find($slug)
+    {
+
+        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
             throw new ModelNotFoundException();
         }
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,21 +4,9 @@
 <link rel="stylesheet" href="/app.css"/>
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My first blog post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My second blog post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My third blog post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis mauris at quam cursus, id semper dolor tempor. Morbi posuere pulvinar risus a tempus.</p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>

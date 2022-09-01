c6e5b58520a00964eec784c7a59e93dd9117dbf2
AliciadW <alicia.dewet@living-stratus.com>
2022-09-01 20:59:02 +0100

    :construction: yield content

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
+@extends('layout')
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css"/>
-
-<body>
+@section('content')
     @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/{{ $post->slug }}" >
+                <a href="/posts/{{ $post->slug }}">
                     {{ $post->title }}
                 </a>
             </h1>
@@ -15,4 +12,4 @@
             <p>{{ $post->excerpt  }}</p>
         </article>
     @endforeach
-</body>
+@endsection('content')

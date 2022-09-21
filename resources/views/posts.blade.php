ae0e91136f3b27bf27970f51828bcb8f5fb49106
AliciadW <alicia.dewet@living-stratus.com>
2022-09-21 20:18:30 +0100

    :construction: the first eloquent relationship

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,9 @@
                     {{ $post->title }}
                 </a>
             </h1>
+            <p>
+                <a href="#"> {{ $post->category->name }}</a>
+            </p>
 
             <p>{{ $post->excerpt  }}</p>
         </article>

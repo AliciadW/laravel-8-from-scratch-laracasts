cd87f3b36a2bea73583b686b8534971d6e762cb2
AliciadW <alicia.dewet@living-stratus.com>
2022-09-27 06:14:01 +0100

    :lipstick: dynamically render posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,38 +1,21 @@
 <x-layout>
-
-
-    {{--    @foreach ($posts as $post)--}}
-    {{--        <article>--}}
-    {{--            <h1>--}}
-    {{--                <a href="/posts/{{ $post->slug }}">--}}
-    {{--                    {{ $post->title }}--}}
-    {{--                </a>--}}
-    {{--            </h1>--}}
-    {{--            <p>--}}
-    {{--                By <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>--}}
-    {{--            </p>--}}
-
-    {{--            <p>{!! $post->excerpt  !!}</p>--}}
-    {{--        </article>--}}
-    {{--    @endforeach--}}
-
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-feature-card :post="$posts[0]"/>
+        @if ($posts->count())
+            <x-post-feature-card :post="$posts[0]"/>
 
-        <div class="lg:grid lg:grid-cols-2">
-{{--            <x-post-card/>--}}
-{{--            <x-post-card/>--}}
-        </div>
+            @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post="$post"/>
+                    @endforeach
+                </div>
+            @endif
 
-        <div class="lg:grid lg:grid-cols-3">
-{{--            <x-post-card/>--}}
-{{--            <x-post-card/>--}}
-{{--            <x-post-card/>--}}
-{{--            <x-post-card/>--}}
-{{--            <x-post-card/>--}}
-        </div>
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
     </main>
 </x-layout>
 

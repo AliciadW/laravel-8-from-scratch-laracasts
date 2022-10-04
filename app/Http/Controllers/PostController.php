38fbbc8eef398a92c59fcbece71f7e3c684497be
AliciadW <alicia.dewet@living-stratus.com>
2022-10-04 19:42:20 +0100

    :art: less code

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {
         return view('posts', [
-            'posts' => $this->getPosts(),
+            'posts' => Post::latest()->filter()->get(),
             'categories' => Category::all()
         ]);
     }
@@ -21,10 +21,5 @@ class PostController extends Controller
         return view('post', [
             'post' => $post
         ]);
-    }
-
-    protected function getPosts()
-    {
-        return Post::latest()->filter()->get();
     }
 }

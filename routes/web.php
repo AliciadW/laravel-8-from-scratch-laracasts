b7794bf02049c7ca81df0bf38448378b467f7e9c
AliciadW <alicia.dewet@living-stratus.com>
2022-10-05 05:21:59 +0100

    :art: write an advanced eloquent query

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,13 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts->load(['category', 'author']),
-        'categories' => Category::all(),
-        'currentCategory' => $category
-    ]);
-});
+//Route::get('categories/{category:slug}', function (Category $category) {
+//    return view('posts', [
+//        'posts' => $category->posts->load(['category', 'author']),
+//        'categories' => Category::all(),
+//        'currentCategory' => $category
+//    ]);
+//});
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [

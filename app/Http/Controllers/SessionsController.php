4e6ca612510d09ace34603c3b6d8172d9c6235cb
AliciadW <alicia.dewet@living-stratus.com>
2022-10-16 16:08:17 +0100

    :lipstick: add login page and functionality

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
     public function login()
     {
+        return view('sessions.create');
+    }
 
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        throw ValidationException::withMessages(['email' => 'Your provided credentials could not be verified.']);
+
+        //return back()
+        //  ->withInput()
+        //  ->withErrors(['email' => 'Your provided credentials could not be verified.']);
     }
 
     public function destroy()

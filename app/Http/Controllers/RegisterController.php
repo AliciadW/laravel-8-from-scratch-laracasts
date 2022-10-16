db2f50a7d42e436b5b6d45e5395f32836d56c516
AliciadW <alicia.dewet@living-stratus.com>
2022-10-16 13:05:10 +0100

    :art: create flash component, shorten flash display code in register controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -26,8 +26,6 @@ class RegisterController extends Controller
 
         User::create($attributes);
 
-        session()->flash('success', 'Your account has been created.');
-
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }

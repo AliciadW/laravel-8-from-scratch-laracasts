db4b0bfab2b3f15b72eb9bbd35365bdb8ddfef49
AliciadW <alicia.dewet@living-stratus.com>
2022-10-17 06:14:27 +0100

    :sparkles: add comments section

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,21 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img class="rounded-xl" src="https://i.pravatar.cc/100" alt="avatar image" width="60"
+             height="60">
+    </div>
+    <div>
+        <header>
+            <h3 class="font-bold">Username</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p class="mt-4">
+            Macaroon tootsie roll cookie shortbread gingerbread. Donut marzipan lemon drops soufflé
+            lemon drops pudding jelly. Donut soufflé tootsie roll biscuit apple pie cookie cotton
+            candy cake. Tiramisu brownie cheesecake cheesecake brownie cheesecake apple pie.
+        </p>
+    </div>
+</article>

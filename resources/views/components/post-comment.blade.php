e58681ae2b21d8f86b8f99aa299960c9361fdf5a
AliciadW <alicia.dewet@living-stratus.com>
2022-10-18 06:44:09 +0100

    :children_crossing: functionality to add a comment, hide comment block when not logged in

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -3,7 +3,8 @@
 <x-panel class="bg-gray-50">
     <article class="flex space-x-4">
         <div class="flex-shrink-0">
-            <img class="rounded-xl" src="https://i.pravatar.cc/100?u={{ $comment -> id }}" alt="avatar image" width="60"
+            <img class="rounded-xl" src="https://i.pravatar.cc/100?u={{ $comment -> user_id }}" alt="avatar image"
+                 width="60"
                  height="60">
         </div>
         <div>
@@ -11,7 +12,7 @@
                 <h3 class="font-bold">{{ $comment->author->username }}</h3>
                 <p class="text-xs">
                     Posted
-                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                    <time>{{ $comment->created_at->format("F j, Y, g:i a") }}</time>
                 </p>
             </header>
 

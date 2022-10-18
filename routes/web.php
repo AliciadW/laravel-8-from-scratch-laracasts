d7356c31ae882263ffe759a38a82a09103fcd642
AliciadW <alicia.dewet@living-stratus.com>
2022-10-18 20:10:11 +0100

    :sparkles: initial work to make use of mailchimp API

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,22 @@ use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('e63037030e', [
+        'email_address' => 'alicia.dewet@gmail.com',
+        'status' => 'subscribed'
+    ]);
+
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);

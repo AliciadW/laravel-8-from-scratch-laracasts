6134d673b74d4dafc8fcd1b0436c1df498201682
AliciadW <alicia.dewet@living-stratus.com>
2022-10-21 06:23:36 +0100

    :passport_control: add admin middleware

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::post('/logout', [SessionsController::class, 'destroy'])->middleware('auth
 
 Route::post('/newsletter', NewsletterController::class);
 
+Route::get('/admin/posts/create', [PostController::class, 'create'])->middleware('admin');
+
 //Route::get('categories/{category:slug}', function (Category $category) {
 //    return view('posts', [
 //        'posts' => $category->posts->load(['category', 'author']),

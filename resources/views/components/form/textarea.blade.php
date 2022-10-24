143ccbe04daacd21db6a9c70d625882015f5378b
AliciadW <alicia.dewet@living-stratus.com>
2022-10-24 07:14:28 +0100

    :construction: edit and delete posts, some cleanup and refactoring

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,12 +1,15 @@
-@props(['name', 'placeholder' => '', 'rows' => 5])
+@props(['name'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
 
-    <textarea class="border border-gray-200 p-2 w-full rounded" name="{{ $name }}" id="{{ $name }}"
-              placeholder="{{ $placeholder }}"
-              rows="{{ $rows }}"
-              required>{{ old($name) }}</textarea>
+    <textarea class="border border-gray-200 p-2 w-full rounded"
+              name="{{ $name }}"
+              id="{{ $name }}"
+              required
+              {{ $attributes }}
+    >{{ $slot ?? old($name) }}
+    </textarea>
 
     <x-form.error name="{{ $name }}"/>
 </x-form.field>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\AdminPostController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 
@@ -23,8 +24,12 @@ Route::post('/logout', [SessionsController::class, 'destroy'])->middleware('auth
 
 Route::post('/newsletter', NewsletterController::class);
 
-Route::get('/admin/posts/create', [PostController::class, 'create'])->middleware('admin');
-Route::post('/admin/posts', [PostController::class, 'store'])->middleware('admin');
+Route::post('/admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
+Route::get('/admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
+Route::get('/admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
+Route::get('/admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
+Route::patch('/admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
+Route::delete('/admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
 
 //Route::get('categories/{category:slug}', function (Category $category) {
 //    return view('posts', [
